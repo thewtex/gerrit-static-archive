5a8e2cc99eb4bd2871914088deeeba66649bea96
Matt McCormick <matt.mccormick@kitware.com>
2018-11-07 18:14:50 -0500

    Add test.sh

--- gerrit-static-archive.py
+++ gerrit-static-archive.py
@@ -6,16 +6,21 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-def main(gerrit_url, output_dir):
+def main(gerrit_url, output_dir, change_set_start, change_set_end):
     os.makedirs(output_dir, exist_ok=True)
 
     change = requests.get(gerrit_url + '/#/c/1')
-    print(change)
+    print(change_set_end)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create static website archive for a Gerrit Code Review instance.')
     parser.add_argument('gerrit_url', help='URL for the Gerrit instance.')
     parser.add_argument('output_dir', help='Output directory for the static website.')
+    parser.add_argument('--change-set-start', '-s', type=int, default=1, help='First Change Set number to archive.')
+    parser.add_argument('--change-set-end', '-e', type=int, default=30000, help='Last Change Set number to archive.')
     args = parser.parse_args()
 
-    main(args.gerrit_url, args.output_dir)
+    print(args)
+
+    main(args.gerrit_url, args.output_dir, args.change_set_start,
+            args.change_set_end)
---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./gerrit-static-archive.py --change-set-start 23649 --change-set-end 23650 'http://review.source.kitware.com' /tmp/gerrit
