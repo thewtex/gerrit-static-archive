3ea8d87bb2f029daee09b98b4a0bcac2383c08a3
Matt McCormick <matt.mccormick@kitware.com>
2018-11-07 18:00:24 -0500

    Parse script arguments

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Create a static website archive a [Gerrit Code
 Review](https://www.gerritcodereview.com/) instance. Keep a record of all
 review discussions and proposed code changes.
 
-Developed and tested against Gerrit 2.12-1-g6f7dc21.
+Developed and tested against Gerrit 2.12-1-g6f7dc21 with Python 3.6.6.
 
 ## Features (todo)
 
--- gerrit-static-archive.py
+++ gerrit-static-archive.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
 import argparse
+import os
 
 import requests
 from bs4 import BeautifulSoup
 
-def main():
-    pass
+def main(gerrit_url, output_dir):
+    os.makedirs(output_dir, exist_ok=True)
+
+    change = requests.get(gerrit_url + '/#/c/1')
+    print(change)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Create static website archive for a Gerrit Code Review instance.')
+    parser.add_argument('gerrit_url', help='URL for the Gerrit instance.')
+    parser.add_argument('output_dir', help='Output directory for the static website.')
+    args = parser.parse_args()
+
+    main(args.gerrit_url, args.output_dir)
