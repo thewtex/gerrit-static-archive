a3c64027569b2bfe6f55fad842d3680b3a09ac60
Matt McCormick <matt.mccormick@kitware.com>
2018-11-18 20:40:59 -0500

    Download some 'link' and 'script' assets

--- gerritbot/gerritbot/spiders/change_number.py
+++ gerritbot/gerritbot/spiders/change_number.py
@@ -15,6 +15,12 @@ class ChangeNumberSpider(scrapy.Spider):
     allowed_domains = ['kitware.com']
     mirror_path = os.path.join('.', 'mirror')
     screenshots_path = os.path.join('.', 'screenshots')
+    site = 'http://review.source.kitware.com'
+
+    def strip_site(self, url):
+        if not url.startswith(self.site):
+            return url
+        return url[len(self.site):]
 
     def start_requests(self):
         change_number_start = int(getattr(self, 'change_number_start', 23828))
@@ -27,24 +33,17 @@ class ChangeNumberSpider(scrapy.Spider):
         if not os.path.exists(change_number_path_s):
             os.makedirs(change_number_path_s)
         for change_number in range(change_number_start, change_number_end + 1):
-            url = 'http://review.source.kitware.com/#/c/' + str(change_number) + '/'
+            url = self.site + '/#/c/' + str(change_number) + '/'
             self.log('url: {}'.format(url))
             yield SeleniumRequest(url=url, callback=self.parse,
                     dont_filter=True,
                     screenshot=True,
                     wait_time=300,
-                    wait_until=EC.element_to_be_clickable((By.ID, 'gerrit_body')))
+                    wait_until=EC.presence_of_element_located((By.XPATH,
+                        '//span[@class="rpcStatus"][@style="display: none;"]')))
 
     def parse(self, response):
         change_number = response.url.split("/")[-2]
-
-        change_path = os.path.join(self.mirror_path, '#', 'c', change_number)
-        if not os.path.exists(change_path):
-            os.mkdir(change_path)
-        body_path = os.path.join(change_path, 'index.html')
-        with open(body_path, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file {}'.format(body_path))
 
         change_path_s = os.path.join(self.screenshots_path, '#', 'c', change_number)
         if not os.path.exists(change_path_s):
@@ -53,3 +52,39 @@ class ChangeNumberSpider(scrapy.Spider):
         with open(screenshot_path, 'wb') as image_file:
             image_file.write(response.meta['screenshot'])
         self.log('Saved file {}'.format(screenshot_path))
+
+        change_path = os.path.join(self.mirror_path, '#', 'c', change_number)
+        if not os.path.exists(change_path):
+            os.mkdir(change_path)
+        html_path = os.path.join(change_path, 'index.html')
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        for link in soup.find_all('link'):
+            url = response.urljoin(str(link['href']))
+            link['href'] = self.strip_site(url)
+            yield scrapy.Request(url=url, callback=self.parse_asset)
+
+        for script in soup.find_all('script'):
+            if script.get('src'):
+                url = response.urljoin(str(script['src']))
+                script['src'] = self.strip_site(url)
+                yield scrapy.Request(url=url, callback=self.parse_asset)
+
+        with open(html_path, 'w') as fp:
+            fp.write(str(soup))
+        self.log('Saved file {}'.format(html_path))
+
+    def parse_asset(self, response):
+        asset = { 'original_url': response.url }
+        url = self.strip_site(response.url)
+        asset['url'] = url
+        asset_path = self.mirror_path + '/' +  os.path.dirname(url)
+        if not os.path.exists(asset_path):
+            os.makedirs(asset_path)
+        asset_path = os.path.join(asset_path, os.path.basename(url))
+        if not os.path.exists(asset_path):
+            with open(asset_path, 'wb') as fp:
+                fp.write(response.body)
+            self.log('Saved file {}'.format(asset_path))
+        item = { 'asset': asset }
+        yield item
