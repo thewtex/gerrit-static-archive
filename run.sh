c2f43e34c119e4f92ec103faea49407bb12eee02
Matt McCormick <matt.mccormick@kitware.com>
2018-12-05 10:49:30 -0500

    ENH: Initial status spider

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ cd gerritbot
 rm output.jl
 mkdir -p mirror
 cp patched/index.html ./mirror/
-scrapy crawl change_number  -o output.jl -a change_number_end=23829
+#scrapy crawl change_number  -o output.jl -a change_number_end=23829
+scrapy crawl status  -o output.jl -a number_end=50
 python ../scripts/extract-changeid-map.py
