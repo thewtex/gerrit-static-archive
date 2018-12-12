23bf44e4aac6b13a07f31a4611cfb7201effb4c0
Matt McCormick <matt.mccormick@kitware.com>
2018-12-12 09:25:36 -0500

    ENH: Download change sets in chunks of 500

--- run.sh
+++ run.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
+set -x
 cd gerritbot
 rm output.jl
 mkdir -p mirror
 cp patched/index.html ./mirror/
-scrapy crawl change_number  -o output.jl -a change_number_end=23829
+change_number_start=1
+change_number_start=2500
+change_number_end=23900
+change_number_chunk_size=500
+change_number_chunk_start=${change_number_start}
+change_number_chunk_end=${change_number_chunk_start}
+let change_number_chunk_end+=${change_number_chunk_size}
+while test ${change_number_chunk_end} -lt ${change_number_end}; do
+  scrapy crawl change_number  -o output.jl \
+    -a change_number_start=${change_number_chunk_start} \
+    -a change_number_end=${change_number_chunk_end}
+  sleep 180
+  let change_number_chunk_start+=${change_number_chunk_size}
+  let change_number_chunk_end+=${change_number_chunk_size}
+done
 python ../scripts/extract-changeid-map.py
